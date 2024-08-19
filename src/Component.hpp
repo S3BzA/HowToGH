40dfb56dcfe7cdc90ef7ad5b503c90fe5411e652
Michael Horwitz <michael@horwitz.co.za>
2024-08-19 09:09:05 +0200

    feat: added comment documentation

--- src/Component.hpp
+++ src/Component.hpp
@@ -3,6 +3,11 @@
 
 #include <string>
 
+/**
+ * The base componenet class for using composite pattern root of the tree
+ * @class Component
+ * @author Michael
+ */
 class Component {
 protected:
   Component *parent_;
@@ -11,6 +16,13 @@ public:
   virtual ~Component() {}
   void SetParent(Component *parent) { this->parent_ = parent; }
   Component *GetParent() const { return this->parent_; }
+  /**
+   * Used to add and remove children from the composite
+   * @param component
+   * @return void
+   * @note This is a pure virtual function
+   * 
+   */
   virtual void Add(Component *component) {}
   virtual void Remove(Component *component) {}
   virtual bool IsComposite() const { return false; }
